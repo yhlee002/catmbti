ce1a75ee04004e15e2e034ce917e9f49e34f8bc5
yhlee <xxxoxxo002@gmail.com>
2022-06-06 03:49:38 +0900

    add Home page
    cf. BootStrap 사용을 위해 react-bootstrap 설치

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,56 @@
 import React from 'react';
-
+import styled from "styled-components";
+import PangImage from '../assets/main-cat.jpg';
+import Button from 'react-bootstrap/Button';
 const Home = () => {
 
   return (
-      <div>헤더입니다.</div>
+      <Wrapper>
+        <Header>예비집사 판별기</Header>
+        <Contents>
+          <Title>나에게 맞는 주인님은?</Title>
+          <LogoImage>
+            <img className="rounded-pill" src={PangImage} width={350} height={520}/>
+          </LogoImage>
+          <Desc>MBTI를 기반으로 하는 나랑 잘맞는 고양이 찾기</Desc>
+          <Button>테스트 시작하기</Button>
+        </Contents>
+      </Wrapper>
   )
 }
 
 export default Home;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100%;
+  
+`
+
+const Header = styled.div`
+  font-size: 40pt;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Contents = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+const Title = styled.div`
+  font-size: 30pt;
+  margin-top: 40px;
+`
+
+const LogoImage = styled.div`
+  margin-top: 10px;
+`
+
+const Desc = styled.div`
+  font-size: 20pt;
+  margin-top: 20px;
+`
