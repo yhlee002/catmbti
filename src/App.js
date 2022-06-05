aa87417ff592bf421337bf31dd5f4bc5059a8794
yhlee <xxxoxxo002@gmail.com>
2022-06-06 04:13:30 +0900

    add package.json

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import Question from "./pages/Question";
+import Result from "./pages/Result";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Home></Home>
+      <Question></Question>
+      <Result></Result>
+
+    </>
+  );
+}
+
+export default App;
